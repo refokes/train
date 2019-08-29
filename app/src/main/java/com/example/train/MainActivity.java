2dc68aafa97064046ef2b6180fcf152dcd026a57
Tan <refokes@gmail.com>
2019-08-28 19:40:01 -0700

    Initial commit

---
+++ app/src/main/java/com/example/train/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.train;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+    }
+
+    private void initViews() {
+        // setting toolbar
+        Toolbar toolbar1 = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar1);
+
+        // setting view pager
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+
+        // setting tabLayout
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        MainFragmentPagerAdapter mainFragmentPagerAdapter = new MainFragmentPagerAdapter(getSupportFragmentManager());
+        mainFragmentPagerAdapter.addFragment(new AboutFragment(), getString(R.string.about));
+        mainFragmentPagerAdapter.addFragment(new HobbyFragment(), getString(R.string.hobby));
+        mainFragmentPagerAdapter.addFragment(new ContactFragment(), getString(R.string.contact));
+        viewPager.setAdapter(mainFragmentPagerAdapter);
+    }
+}
